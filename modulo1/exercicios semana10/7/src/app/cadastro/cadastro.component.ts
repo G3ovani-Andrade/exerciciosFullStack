e4aa877829c0ddcbedbe9aec26993821f39f2d44
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-09-21 11:22:56 -0300

    modificacao cadastro usando reactive forms

--- modulo1/exercicios semana10/7/src/app/cadastro/cadastro.component.ts
+++ modulo1/exercicios semana10/7/src/app/cadastro/cadastro.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Usuario } from '../usuario/Usuario';
 import { StorageServiceService } from '../services/storage/storage-service.service';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,67 +9,32 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
-  numero: boolean = false;
-  minuscula: boolean = false;
-  maiuscula: boolean = false;
-  especial: boolean = false;
   usuarios: any = [];
-  confimarSenha: string = "";
   mensagem: string = '';
-  loginFormModel: Usuario = {
-    usuario: '',
-    senha: ''
-  }
 
   cadastroForm:FormGroup;
 
   constructor(private storage: StorageServiceService) {
     this.cadastroForm = new FormGroup({
-      nome_usuario:new FormControl('',[Validators.required]),
-      senha:new FormControl('',[Validators.required,Validators.minLength(8)]),
-      confirmacao_senha:new FormControl('',[Validators.required,Validators.minLength(8)]),
+      usuario:new FormControl('',[Validators.required]),
+      senha:new FormControl('',[]),
+      confirmacao_senha:new FormControl('',[]),
     });
+    this.cadastroForm.get('usuario')?.setValidators([Validators.required,this.checarDisponibilidadeUsuario()]);
+    this.cadastroForm.get('senha')?.setValidators([Validators.required,Validators.minLength(8),this.checarSenha(),this.checarConfirmacaoSenha()]);
+    this.cadastroForm.get('confirmacao_senha')?.setValidators([Validators.required,Validators.minLength(8),this.checarSenha(),this.checarConfirmacaoSenha()]);
+
   }
 
-  ngOnInit() {
-    this.criarForm();
-  }
-  criarForm() {
-    this.loginFormModel = {
-      usuario: "",
-      senha: "",
-    }
-  }
 
-  checar(senha:string) {
-    this.maiuscula = /^(?=.*[A-Z])/.test(senha);
-    this.minuscula = /^(?=.*[a-z])/.test(senha);
-    this.numero = /^(?=.*[0-9])/.test(senha);
-    this.especial = /^(?=.*[!@#$%^&*()_+\-=[\]{};'¨:"\|,.<>/?])/.test(senha);
-  }
 
   cadastrar() {
-    let senha = this.cadastroForm.get('senha')?.value;
-    this.checar(senha);
-    this.limparMensagem();
-    if (this.loginFormModel.senha != this.confimarSenha) {
-      this.mensagem = "As senhas são diferentes"
-    }
-    if (!(this.maiuscula && this.minuscula && this.especial && this.numero && !(this.loginFormModel.senha.length < 8))) {
-      return;
-    } else {
-      this.usuarios = []
-      this.usuarios = this.storage.getLocalStorage('USUARIO');
-      if (this.checarUsuario(this.usuarios)) {
-        this.usuarios.push(this.loginFormModel);
-        this.storage.setLocalStorage('USUARIO', this.usuarios);
-        this.criarForm();
-        this.confimarSenha = "";
-        this.mensagem = "cadastrado com sucesso"
-      } else {
-        this.mensagem = 'Usuário ja cadastrado'
-      }
-    }
+      this.usuarios.push(
+        {
+        usuario:this.cadastroForm.get('usuario')?.value,
+        senha:this.cadastroForm.get('senha')?.value
+      });
+      this.storage.setLocalStorage('USUARIO', this.usuarios);
   }
 
   limparMensagem(){
@@ -78,13 +43,52 @@ export class CadastroComponent {
     }, 2000);
   }
 
-  checarUsuario(usuarios: Usuario[]) {
-    let novoArray = usuarios.find((usuario) => JSON.stringify(usuario.usuario) === JSON.stringify(this.loginFormModel.usuario));
+  checarUsuario() {
+    this.usuarios = [];
+    this.usuarios = this.storage.getLocalStorage('USUARIO');
+    let novoArray = this.usuarios.find((usuario:Usuario) => JSON.stringify(usuario.usuario) === JSON.stringify(this.cadastroForm.get("usuario")?.value));
     if (novoArray) {
-      console.log(novoArray);
-      return false;
+      return true;
     } else {
-      return true
+      return false;
     }
   }
+
+  checarSenha(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+        const senha = control.value;
+        if (!senha) {
+            return null;
+        }
+        const hasMaiuscula = /[A-Z]+/.test(senha);
+        const hasMinuscula = /[a-z]+/.test(senha);
+        const hasNumero = /[0-9]+/.test(senha);
+        const hasCaractereEspecial =  /[!@#$%^&*()_+\-=[\]{};'¨:"\|,.<>/?]+/.test(senha);
+        const senhaValida = hasMaiuscula && hasMinuscula && hasNumero && hasCaractereEspecial;
+        return !senhaValida ? {senhaValidaRetorno:true}: null;
+    }
 }
+
+checarConfirmacaoSenha():ValidatorFn{
+  return():ValidationErrors | null=>{
+    const senha = this.cadastroForm.get('senha')?.value;
+    const confirmacaoSenha = this.cadastroForm.get('confirmacao_senha')?.value;
+    if(!confirmacaoSenha || !senha){
+      return null;
+    }
+    return confirmacaoSenha!==senha ?{senhaCorrespondente:true} :null;
+  }
+}
+
+checarDisponibilidadeUsuario():ValidatorFn{
+  return():ValidationErrors | null=>{
+      let usuaioExiste = this.checarUsuario()
+      if (usuaioExiste) {
+        return {usuarioExiste:true};
+      }
+    return null
+  }
+}
+
+}
+
--- modulo1/exercicios semana10/7/src/app/login/login.component.ts
+++ modulo1/exercicios semana10/7/src/app/login/login.component.ts
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
       this.mensagem = 'Usuário ou senha inválido'
     }else{
       this.rotas.navigate(['/listagem'])
-    }
+   }
 
   }
 
